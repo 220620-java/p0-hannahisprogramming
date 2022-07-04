c949433b527983ce8b2ffcf5abff651551a2f48e
Hannah Bush <heb.rutgers@gmail.com>
2022-07-04 14:28:11 -0400

    removed self-made list; added userDAO

--- BankApp/src/main/BankApp/data/UserDao.java
+++ BankApp/src/main/BankApp/data/UserDao.java
@@ -1,5 +1,8 @@
 package main.BankApp.data;
 
-public interface UserDao {
+import main.BankApp.models.User;
 
+public interface UserDao {
+    public User findByUsername(String username);
+    public User create(User user);
 }
