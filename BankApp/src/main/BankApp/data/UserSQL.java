1293adb706f620a5e86979c3607f7b6f1b22af62
Hannah Bush <heb.rutgers@gmail.com>
2022-07-04 15:06:52 -0400

    doing this wrong, but changed db connection

--- BankApp/src/main/BankApp/data/UserSQL.java
+++ BankApp/src/main/BankApp/data/UserSQL.java
@@ -1,4 +1,20 @@
 package main.BankApp.data;
 
-public class UserSQL {
+import main.BankApp.db.Connect;
+import main.BankApp.models.User;
+
+import java.sql.PreparedStatement;
+
+public class UserSQL implements UserDao {
+    private Connect connection = Connect.connectToDb();
+
+    @Override
+    public User create(User user) {
+        try (connection) {
+            String sql = "insert into bank.users" + "(id, username, password)" + "values (default, ?, ?)";
+            String[] keys = {"id"};
+
+            PreparedStatement stmt = connection.prepareStatement(sql, keys);
+        }
+    }
 }
