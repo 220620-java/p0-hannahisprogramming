e4f5e404c53d4c7821a44b4a60b0a0790006e7e2
Hannah Bush <heb.rutgers@gmail.com>
2022-07-04 13:21:18 -0400

    arrayList - add methods

--- BankApp/src/main/BankApp/utils/ArrayList.java
+++ BankApp/src/main/BankApp/utils/ArrayList.java
@@ -1,4 +1,30 @@
 package main.BankApp.utils;
 
-public interface ArrayList {
+public class ArrayList<X> implements List<X>{
+    private X[] array;
+    private int nextIndex;
+    public ArrayList() {this.array = (X[]) new Object[10];}
+
+    // add methods
+    @Override
+    public void add(X obj) {
+        this.array[nextIndex] = obj;
+        nextIndex++;
+    }
+
+    @Override
+    public void add(X obj, int index) {
+        for(int i = this.nextIndex-1; i >= index; i--) {
+            this.array[i+1] = this.array[i];
+        }
+        this.array[index] = obj;
+        nextIndex++;
+    }
+
+    @Override
+    public void addAll(X... objs) {
+        for(X obj : objs) {
+            this.add(obj);
+        }
+    }
 }
