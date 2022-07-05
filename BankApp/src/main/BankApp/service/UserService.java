dc3210aac142f31124ed7d6db0200c1a0bbdd282
Hannah Bush <heb.rutgers@gmail.com>
2022-07-05 16:55:39 -0400

    user service added

--- BankApp/src/main/BankApp/service/UserService.java
+++ BankApp/src/main/BankApp/service/UserService.java
@@ -1,4 +1,21 @@
 package main.BankApp.service;
 
+import main.BankApp.data.interfaces.UserDao;
+import main.BankApp.data.methods.UserSQL;
+import main.BankApp.models.User;
+
 public class UserService {
+    private UserDao userDao;
+    public UserService() { userDao = new UserSQL(); }
+
+    //add username exception
+    //@Override
+    public User create(int id, String name, String username, String password) {
+        User user;
+        user = new User(id, name, username, password);
+        return user;
+    }
+
+    //@Override
+    public User findByUsername(String username) {return userDao.findByUsername(username);}
 }
