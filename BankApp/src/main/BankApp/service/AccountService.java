c2e46d9b63527ff52a766709f71126fa9a03178f
Hannah Bush <heb.rutgers@gmail.com>
2022-07-05 17:38:01 -0400

    account service created

--- BankApp/src/main/BankApp/service/AccountService.java
+++ BankApp/src/main/BankApp/service/AccountService.java
@@ -1,5 +1,27 @@
 package main.BankApp.service;
 
-public class AccountService {
+import main.BankApp.data.interfaces.AccountDao;
+import main.BankApp.data.methods.AccountSQL;
+import main.BankApp.models.Account;
+import main.BankApp.models.User;
 
+public class AccountService {
+    private AccountDao accountDao;
+    public AccountService() { accountDao = new AccountSQL(); }
+
+
+    public Account createAccount(Account account, String type, double balance, User user) {
+        accountDao.create(account, type, balance, user);
+        return account;
+    }
+
+    public Account getAccount(Account account, User user) {
+        accountDao.get(account, user);
+        return account;
+    }
+
+    public Account updateBalance(Account account, double balance, double amount) {
+        accountDao.updateBalance(account, balance, amount);
+        return account;
+    }
 }
